c3d242368efd79fbe20c5f254b4bc7d68de19fab
Marko <markochoi1010@gmail.com>
2025-02-10 21:00:07 +0800

    [CoC-1] fine-tuning screen annotation interaction

--- cocreate-qualtrics/dist/static/cocreate-new.js
+++ cocreate-qualtrics/dist/static/cocreate-new.js
@@ -14785,7 +14785,7 @@ const Canvas = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       redrawSelections(ctx);
       drawSelection(ctx, startX, startY, width2, height2);
-      handleMouseUp();
+      handleMouseUp(e);
     }
     setSelectionStart(null);
     setSelectionEnd(null);
@@ -14797,26 +14797,33 @@ const Canvas = () => {
     const x = e.clientX;
     const y = e.clientY;
     if (x < rect.left || x > rect.right || y < rect.top || y > rect.bottom) {
-      handleMouseUp();
+      handleMouseUp(e);
       return true;
     }
     return false;
   };
-  const handleMouseUp = () => {
+  const handleMouseUp = (e) => {
     if (!isSelecting || !selectionStart || !selectionEnd || !canvasRef.current) return;
     let sameXCoordinate = selectionStart.x === selectionEnd.x;
     let sameYCoordinate = selectionStart.y === selectionEnd.y;
-    if (sameXCoordinate && sameYCoordinate && !isEnteringFeedback && allowPictureSelection) {
-      console.log("Creating picture-wide selection");
-      const canvasElement = canvasRef.current;
-      if (!canvasElement) return;
-      const { width: width2, height: height2 } = canvasElement.getBoundingClientRect();
-      if (!canCreatePictureSelection(width2, height2)) {
+    console.log("Same X Coordinate: " + sameXCoordinate);
+    console.log("Same Y Coordinate: " + sameYCoordinate);
+    console.log("Selection Start: " + JSON.stringify(selectionStart), "\nSelection End: " + JSON.stringify(selectionEnd));
+    if (sameXCoordinate && sameYCoordinate) {
+      if (!isEnteringFeedback && allowPictureSelection) {
+        const canvasElement = canvasRef.current;
+        if (!canvasElement) return;
+        const { width: width2, height: height2 } = canvasElement.getBoundingClientRect();
+        if (!canCreatePictureSelection(width2, height2)) {
+          return;
+        }
+        createPictureSelection(width2, height2);
+        setIsEnteringFeedback(true);
+        setAllowPictureSelection(false);
+      } else {
+        openPictureSelectionFeedback(e);
         return;
       }
-      createPictureSelection(width2, height2);
-      setIsEnteringFeedback(true);
-      setAllowPictureSelection(false);
     } else {
       createNewSelection(selectionStart, selectionEnd);
       setIsEnteringFeedback(true);
@@ -14831,6 +14838,37 @@ const Canvas = () => {
     setIsSelecting(false);
     setSelectionStart(null);
     setSelectionEnd(null);
+  };
+  const openPictureSelectionFeedback = (e) => {
+    console.log("Opening picture-wide selection feedback");
+    setIsEnteringFeedback(true);
+    const canvasElement = canvasRef.current;
+    if (!canvasElement) return;
+    const { width: width2, height: height2 } = canvasElement.getBoundingClientRect();
+    const pictureSelection = selections.find((selection) => {
+      return selection.start.x === 0 && selection.start.y === 0 && selection.end.x === width2 && selection.end.y === height2;
+    });
+    console.log("Picture-wide selection found: " + JSON.stringify(pictureSelection));
+    if (pictureSelection) {
+      const pictureSelectionIndex = selections.indexOf(pictureSelection);
+      const mouseCoordinates = getMouseCoordinates(e);
+      console.log("Opening tooltip at: \n" + JSON.stringify(mouseCoordinates));
+      setActiveSelectionIndex(pictureSelectionIndex);
+      setTooltipPosition(mouseCoordinates);
+      setIsSelecting(false);
+      setSelectionStart(null);
+      setSelectionEnd(null);
+    }
+  };
+  const getMouseCoordinates = (e) => {
+    const defaultCoordinates = { x: 0, y: 0 };
+    const canvas = canvasRef.current;
+    if (!e || !canvas) {
+      return defaultCoordinates;
+    }
+    const x = e.clientX;
+    const y = e.clientY;
+    return { x, y };
   };
   const canCreatePictureSelection = (width2, height2) => {
     let pictureWideSelection = selections.filter((selection) => {
@@ -14847,11 +14885,11 @@ const Canvas = () => {
     setSelections((prev2) => [...prev2, newSelection]);
   };
   const createPictureSelection = (pictureWidth, pictureHeight) => {
-    console.log("Creating picture-wide selection");
     const newSelection = {
       start: { x: 0, y: 0 },
       end: { x: pictureWidth, y: pictureHeight }
     };
+    console.log("Creating picture-wide selection: \n" + JSON.stringify(newSelection));
     setSelections((prev2) => [...prev2, newSelection]);
   };
   const handleEdit = (index) => {
