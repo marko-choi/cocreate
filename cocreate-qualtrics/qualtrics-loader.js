46db245d109a417c3aa3c9c2e7df4279deea1ffc
Marko <markochoi1010@gmail.com>
2025-03-28 12:31:23 +0800

    fix qualtrics SPA update

--- cocreate-qualtrics/qualtrics-loader.js
+++ cocreate-qualtrics/qualtrics-loader.js
@@ -132,7 +132,7 @@ async function loadReactApp(qualtricsSurveyEngine) {
 				rootDiv.style.alignItems = 'center';
 				rootDiv.style.justifyContent = 'center';
 				rootDiv.style.overflow = 'visible';
-				rootDiv.style.height = '72.5vh';
+				rootDiv.style.height = '65vh';
 			}	
 
 			console.log('React app loaded!');
