3fda3e6200b908ba5c61517ac13012cb64cc8949
Marko <markochoi1010@gmail.com>
2025-03-29 22:24:11 +0800

    render multiple instances of cocreate-root

--- cocreate-qualtrics/dist/static/cocreate-new.js
+++ cocreate-qualtrics/dist/static/cocreate-new.js
@@ -14716,12 +14716,12 @@ const Canvas = (props) => {
       console.log("[Cocreate] No root container found for instanceId: " + instanceId);
       return;
     }
-    const questionId = instanceRootContainer.getAttribute("data-question-id");
-    const questionBodyImage = instanceRootContainer.querySelector(`#question-${questionId} img`);
+    const questionBodyImage = instanceRootContainer.querySelector(`.question-content img`);
     console.log("[Cocreate] Question Body Image: " + questionBodyImage);
     if (questionBodyImage && questionBodyImage instanceof HTMLImageElement) {
       console.log("[Cocreate] Scraping image from question body");
       console.log("[Cocreate] Question Body Image: " + questionBodyImage);
+      console.log("[Cocreate] Question Body Image Src: " + questionBodyImage.getAttribute("src"));
       setImageSrc(questionBodyImage.getAttribute("src") ?? DEFAULT_IMAGE_SRC);
       var loadedImage = void 0;
       while (!loadedImage) {
--- cocreate/src/components/canvas/Canvas.tsx
+++ cocreate/src/components/canvas/Canvas.tsx
@@ -199,9 +199,8 @@ const Canvas: React.FC<CanvasProps> = (props) => {
       console.log("[Cocreate] No root container found for instanceId: " + instanceId);
       return;
     }
-      
-    const questionId = instanceRootContainer.getAttribute("data-question-id")
-    const questionBodyImage = instanceRootContainer.querySelector(`#question-${questionId} img`);
+
+    const questionBodyImage = instanceRootContainer.querySelector(`.question-content img`);
     console.log("[Cocreate] Question Body Image: " + questionBodyImage);
 
     if (questionBodyImage && questionBodyImage instanceof HTMLImageElement) {
